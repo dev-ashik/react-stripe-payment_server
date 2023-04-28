fbb1e6662a98e2222b4532a97d540846bde91f32
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-04-28 12:53:49 +0800

    payment successful

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 var cors = require('cors')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 
 
 // middlewares
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 // to read json 
@@ -18,6 +23,29 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Server is runing..')
 })
+
+app.post('/payment', async (req, res) => {
+    let status, error;
+    
+    const {token, amount } = req.body;
+    console.log(token)
+
+    try {
+        await stripe.charges.create({
+            source: token.id,
+            amount,
+            currency: 'usd'
+        });
+
+        status = "success"
+    } catch(error) {
+         console.log(error)
+         status = "fail"
+    }
+
+    res.json({error, status})
+})
+
 
 
 
